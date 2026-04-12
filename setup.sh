ea2752ab40a35d31fd13e1207c64041513d378f2
Ayaan496 <mushtaqa@vcu.edu>
2026-04-12 12:10:05 -0400

    added some aliases

---
+++ setup.sh
@@ -0,0 +1,13 @@
+cat << 'EOF' >> ~/.bashrc
+
+# URC ROS aliases
+alias cb='colcon build'
+alias cbs='colcon build --packages-select'
+alias urc='cd ~/URC/urc_ws_interbotix && source /opt/ros/jazzy/setup.bash && source install/setup.bash'
+alias source_urc='source /opt/ros/jazzy/setup.bash && source ~/URC/urc_ws_interbotix/install/setup.bash'
+alias launch_arm='ros2 launch interbotix_xsarm_control xsarm_control.launch.py robot_model:=wx250'
+alias launch_urc='ros2 launch receiver receiver_launch.py'
+EOF
+
+source ~/.bashrc
+echo "URC setup complete"
